b6d377e3d88216097637c9dfc1594f9aab2ca52e
godspeedglory <maamarkina@gmail.com>
2026-01-29 19:27:11 +0300

    meow3/main.go
    3 номер

---
+++ meow1/meow3/main.go
@@ -0,0 +1,20 @@
+package main
+import (
+	"strconv"
+)
+func adding (a,b string) int64{
+	tolkoCifri:= func(s string) string{
+		result:=""
+		for i:=0;i<len(s);i++{
+			if s[i]>='0' && s[i]<='9'{
+				result=result+string(s[i])
+			}
+		}
+		return result
+	}
+	clean1:=tolkoCifri(a)
+	clean2:=tolkoCifri(b)
+	chislo1,_:=strconv.ParseInt(clean1,10,64)
+	chislo2,_:=strconv.ParseInt(clean2,10,64)
+	return chislo1+chislo2
+}
