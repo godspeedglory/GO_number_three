25b12d4cc0bb7dd164e40659df34c5bb2d6a5162
godspeedglory <maamarkina@gmail.com>
2026-01-29 19:52:21 +0300

    Create num2.go
    2

---
+++ meow1/num2.go
@@ -0,0 +1,15 @@
+package main
+import "fmt"
+func convert(x int64) uint16 {
+    if x < 0 {
+		panic("отрицательное число")
+    }
+    if x > 65535 {  
+		panic("число слишком большое")
+    }
+    return uint16(x)
+}
+func main(){
+	var chislo int64
+	fmt.Scan(&chislo)
+	fmt.Print(convert(chislo))
